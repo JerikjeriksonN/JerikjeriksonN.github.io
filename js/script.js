2e956f7d22fc3bab0f1ad96b6845c037d9d04310
JerikjeriksonN <erik.magnifico@gmail.com>
2026-07-25 21:14:59 +0200

    Basic building and fixes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 console.log("Website loaded");
+
+const slides = document.querySelectorAll(".slide");
+
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+let current = 0;
+
+
+next.addEventListener("click", () => {
+
+    slides[current].classList.remove("active");
+
+    current = (current + 1) % slides.length;
+
+    slides[current].classList.add("active");
+
+});
+
+
+prev.addEventListener("click", () => {
+
+    slides[current].classList.remove("active");
+
+    current = (current - 1 + slides.length) % slides.length;
+
+    slides[current].classList.add("active");
+
+});
