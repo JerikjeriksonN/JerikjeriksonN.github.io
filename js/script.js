1aa4fb0bb1e9a25dd13911799b548f9f6cd1740a
JerikjeriksonN <erik.magnifico@gmail.com>
2026-07-27 00:09:03 +0200

    picture slider numbers

--- js/script.js
+++ js/script.js
@@ -1,37 +1,49 @@
-console.log("Website loaded");
-
-
-const sliders = document.querySelectorAll(".slider");
-
-
-sliders.forEach(slider => {
+document.querySelectorAll(".slider").forEach(slider => {
 
     const slides = slider.querySelectorAll(".slide");
-
     const next = slider.querySelector(".next");
     const prev = slider.querySelector(".prev");
+    const counter = slider.querySelector(".slide-counter");
 
     let current = 0;
 
 
-    next.addEventListener("click", () => {
+    function updateSlider() {
 
-        slides[current].classList.remove("active");
-
-        current = (current + 1) % slides.length;
+        slides.forEach(slide => {
+            slide.classList.remove("active");
+        });
 
         slides[current].classList.add("active");
+
+       counter.textContent = 
+       `${String(current + 1).padStart(2,'0')} / ${String(slides.length).padStart(2,'0')}`;
+
+    }
+
+
+    next.addEventListener("click", () => {
+
+        current++;
+
+        if (current >= slides.length) {
+            current = 0;
+        }
+
+        updateSlider();
 
     });
 
 
     prev.addEventListener("click", () => {
 
-        slides[current].classList.remove("active");
+        current--;
 
-        current = (current - 1 + slides.length) % slides.length;
+        if (current < 0) {
+            current = slides.length - 1;
+        }
 
-        slides[current].classList.add("active");
+        updateSlider();
 
     });
 
